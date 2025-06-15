f4387d59593aa59e83c76d0f1afa47f39d1c335e
Will Griffioen <willgriff19@gmail.com>
2025-06-14 19:07:55 -0600

    refactor: rename border color to customBorder for Tailwind debug

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -29,7 +29,7 @@ const config: Config = {
           800: '#1f2937',
           900: '#111827',
         },
-        border: '#e5e7eb',
+        customBorder: '#e5e7eb',
         input: 'hsl(var(--input))',
         ring: 'hsl(var(--ring))',
         background: 'hsl(var(--background))',
